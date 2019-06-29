5d481f3a6c3c0c87501e5a814a08b5213c710e58
hanubathala <hanu.bathala@gmail.com>
2019-06-29 09:05:34 +0530

    Image list

--- TattwavidanandajiBookStoreAdmin/TattwavidanandajiBookStoreAdmin/UI/js/BookList.js
+++ TattwavidanandajiBookStoreAdmin/TattwavidanandajiBookStoreAdmin/UI/js/BookList.js
@@ -1,5 +1,118 @@
-﻿var app = angular.module('myApp', ['ngStorage', 'ui.bootstrap']);
-var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $interval) {
+﻿var app = angular.module('myApp', ['ngStorage', 'ui.bootstrap', 'angularFileUpload']);
+app.directive('file-input', function ($parse) {
+    return {
+        restrict: "EA",
+        template: "<input type='file' />",
+        replace: true,
+        link: function (scope, element, attrs) {
+
+            var modelGet = $parse(attrs.fileInput);
+            var modelSet = modelGet.assign;
+            var onChange = $parse(attrs.onChange);
+
+            var updateModel = function () {
+                scope.$apply(function () {
+                    modelSet(scope, element[0].files[0]);
+                    onChange(scope);
+                });
+            };
+
+            element.bind('change', updateModel);
+        }
+    };
+});
+app.directive("ngFileSelect", function () {
+
+    return {
+
+        link: function ($scope, el) {
+
+            el.on('click', function () {
+
+                this.value = '';
+
+            });
+
+            el.bind("change", function (e) {
+
+                $scope.file = (e.srcElement || e.target).files[0];
+
+
+
+                var allowed = ["jpeg", "png", "gif", "jpg"];
+
+                var found = false;
+
+                var img;
+
+                img = new Image();
+
+                allowed.forEach(function (extension) {
+
+                    if ($scope.file.type.match('image/' + extension)) {
+
+                        found = true;
+
+                    }
+
+                });
+
+                if (!found) {
+
+                    alert('file type should be .jpeg, .png, .jpg, .gif');
+
+                    return;
+
+                }
+
+                img.onload = function () {
+
+                    var dimension = $scope.selectedImageOption.split(" ");
+
+                    if (dimension[0] == this.width && dimension[2] == this.height) {
+
+                        allowed.forEach(function (extension) {
+
+                            if ($scope.file.type.match('image/' + extension)) {
+
+                                found = true;
+
+                            }
+
+                        });
+
+                        if (found) {
+
+                            if ($scope.file.size <= 1048576) {
+
+                                $scope.getFile();
+
+                            } else {
+
+                                alert('file size should not be grater then 1 mb.');
+
+                            }
+
+                        } else {
+
+                            alert('file type should be .jpeg, .png, .jpg, .gif');
+
+                        }
+
+                    } else {
+
+                        alert('selected image dimension is not equal to size drop down.');
+
+                    }
+
+                };
+                //  img.src = _URL.createObjectURL($scope.file);
+            });
+        }
+    };
+});
+
+var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $interval, fileReader) {
     if ($localStorage.uname == null) {
         window.location.href = "BooksList.html";
     }
@@ -20,7 +133,23 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $int
         });
 
     }
+    $scope.onFileSelect1 = function () {
 
+        fileReader.readAsDataUrl($scope.file, $scope).then(function (result) {
+
+            $scope.BookImage = result;
+        });
+    }
+    $scope.UploadImg = function () {
+        var fileinput = document.getElementById('fileInput');
+        fileinput.click();
+
+        //  
+        //if ($scope.file == null)
+        //{ $scope.file = fileinput.files[0]; }
+        //fileReader.readAsDataUrl($scope.file, $scope).then(function (result) { $scope.imageSrc = result; });
+        //fileReader.onLoad($scope.file, $scope).then(function (result) { $scope.imageSrc = result; });
+    };
     $scope.AddNewBook = function (Book, flag) {
         if (Book.BookTitle == null) {
             alert('Please Enter Details.');
@@ -49,7 +178,7 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $int
             BookStock: Book.BookStock,
             BookType: Book.BookType.Id,
             BookDescription: Book.BookDescription,
-            BookImage:Book.BookImage,
+            BookImage: ($scope.BookImage == null) ? null : $scope.BookImage,
             flag: flag
         };
         var req = {
@@ -80,7 +209,127 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $int
             }
         }
     }
+    $scope.openimagetoshow = function (t) {
 
+        openPDF(t.BookImage, "BookImage");
+    }
+    function openPDF(resData, fileName) {
+
+        var blob = null;
+        var ext = fileName.split('.').pop();
+        if (ext == 'csv') {
+            blob = new Blob([resData], { type: "text/csv" });
+            saveAs(blob, fileName);
+        }
+        else {
+
+            var ieEDGE = navigator.userAgent.match(/Edge/g);
+            var ie = navigator.userAgent.match(/.NET/g); // IE 11+
+            var oldIE = navigator.userAgent.match(/MSIE/g);
+
+            if (ie || oldIE || ieEDGE) {
+                blob = b64toBlob(resData, (ext == 'csv') ? 'text/csv' : 'application/pdf');
+                // window.open(blob, '_blank');
+                //  window.navigator.msSaveBlob(blob, fileName);
+                saveAs(blob, fileName);
+                //openReportWindow('test', resData, 1000, 700);
+                //window.open(resData, '_blank');
+                //  var a = document.createElement("a");
+                //  document.body.appendChild(a);
+                //  a.style = "display: none";
+                //  a.href = resData;
+                //  a.download = fileName;
+                ////  a.onclick = "window.open(" + fileURL + ", 'mywin','left=200,top=20,width=1000,height=800,toolbar=1,resizable=0')";
+                //  a.click(); 
+
+            }
+            else {
+
+                if (ext == 'csv' || ext == 'pdf') {
+                    blob = b64toBlob(resData, (ext == 'csv') ? 'text/csv' : 'application/pdf');
+                    saveAs(blob, fileName);
+                }
+                else {
+                    openReportWindow(fileName, resData, 1000, 700);
+                }
+                // newWindow =   window.open(resData, 'newwin', 'left=200,top=20,width=1000,height=700,toolbar=1,resizable=0');
+                //   timerObj = window.setInterval("ResetTitle('"+fileName+"')", 10);
+            }
+        }
+    }
+
+    var winLookup;
+    var showToolbar = false;
+    function openReportWindow(m_title, m_url, m_width, m_height) {
+        var strURL;
+        var intLeft, intTop;
+
+        strURL = m_url;
+
+        // Check if we've got an open window.
+        if ((winLookup) && (!winLookup.closed))
+            winLookup.close();
+
+        // Set up the window so that it's centered.
+        intLeft = (screen.width) ? ((screen.width - m_width) / 2) : 0;
+        intTop = 20;//(screen.height) ? ((screen.height - m_height) / 2) : 0;
+
+        // Open the window.
+        winLookup = window.open('', 'winLookup', 'scrollbars=no,resizable=yes,toolbar=' + (showToolbar ? 'yes' : 'no') + ',height=' + m_height + ',width=' + m_width + ',top=' + intTop + ',left=' + intLeft);
+        checkPopup(m_url, m_title);
+
+        // Set the window opener.
+        if ((document.window != null) && (!winLookup.opener))
+            winLookup.opener = document.window;
+
+        // Set the focus.
+        if (winLookup.focus)
+            winLookup.focus();
+    }
+
+    function checkPopup(m_url, m_title) {
+        if (winLookup.document) {
+            //identify the file type and display accordingly
+            var ext = m_title.split('.').pop();
+            switch (ext) {
+                case 'pdf':
+                    winLookup.document.write('<html><head><title>' + m_title + '</title></head><body height="100%" width="100%"><embed type="application/pdf" src="' + m_url + '" height="100%" width="100%" /></body></html>');
+                    break;
+                default:
+                    winLookup.document.write('<html><head><title>' + m_title + '</title></head><body height="100%" width="100%"><img src="' + m_url + '" height="100%" width="100%" /></body></html>');
+                    break;
+            }
+
+        } else {
+            // if not loaded yet
+            setTimeout(checkPopup(m_url, m_title), 10); // check in another 10ms
+        }
+    }
+
+    function b64toBlob(b64Data, contentType) {
+        contentType = contentType || '';
+        var sliceSize = 512;
+        b64Data = b64Data.replace(/^[^,]+,/, '');
+        b64Data = b64Data.replace(/\s/g, '');
+        var byteCharacters = window.atob(b64Data);
+        var byteArrays = [];
+
+        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            var byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+        var blob = new Blob(byteArrays, { type: contentType });
+        return blob;
+    }
    });
 
 
